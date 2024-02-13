79ac566c89f261017d01d80d866271aff3ab7de7
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2024-02-13 10:11:02 +0100

    I added calculation to the code

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -125,10 +125,28 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  //cal item packed
+  const numPacked = items.filter((item) => item.packed).length;
+  // calc percentage
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className='stats'>
-      <em>You have X items on your list, and you already packed X (X%)</em>{' '}
+      <em>
+        {percentage === 100
+          ? 'You got everything! Ready to go'
+          : `You have ${numItems} items on your list, and you already packed{' '}
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
